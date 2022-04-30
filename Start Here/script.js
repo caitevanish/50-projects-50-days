7a2cdd388f58af8a54e585e9fce598d4de614c9a
caitevanish <cait.evan@gmail.com>
2022-04-29 22:22:49 -0700

    clean up code

--- Start Here/script.js
+++ Start Here/script.js
@@ -1,8 +1,9 @@
 // Main idea: Change the class on each of the panels
+
 // #1 Bring in all panels into the javascript file
 // #2 Have an event listener on each of the panels sop when they;r clicke dsomething happends
 // #3 classList = list of the classes. When you add '.add' method, adds the 'class' into class list
-// # Make a function to remove the active class when it is assigned
+// #4 Make a function to remove the active class when it is assigned
 // #
 
 const panels = document.querySelectorAll('.panel') //puts all panels into a node list (similar to an array)
